64db17f8a67fadb1d249a5b464a84ac08376cdbd
Valery Vargin <VDVUGaD@gmail.com>
2018-06-03 22:11:10 +0300

    Removed d.ts files from lib

--- lib/Container.ts
+++ lib/Container.ts
@@ -1,4 +1,3 @@
-import {ContainerInterface, Definition, Options} from "./ContainerInterface";
 import {
     AlreadyDefinedException,
     CreateNotCallableException,
@@ -7,12 +6,95 @@ import {
     NotFoundException
 } from "./Exception";
 
+export namespace Options {
+    export interface Factory {
+        createEveryTime?: boolean
+    }
+
+    export interface Class extends Factory {
+        constructorArguments?: void | any[] | string[]
+    }
+}
+
+export namespace Definition {
+    export enum Types {
+        Scalar = 'scalar',
+        Factory = 'factory',
+    }
+
+    export interface Definition {
+        type: string
+    }
+
+    export interface Scalar extends Definition {
+        value: any
+    }
+
+    export interface Factory extends Definition {
+        factory: (container: ContainerInterface) => any
+        options: Options.Factory
+    }
+}
+
 /**
- * @param id
+ * Describes the interface of a container that exposes methods to read its entries
+ */
+export interface PsrContainerInterface {
+    /**
+     * Finds an entry of the container by its identifier and returns it
+     *
+     * @param {string} id
+     *
+     * @throws NotFoundExceptionInterface  No entry was found for **this** identifier.
+     * @throws ContainerExceptionInterface Error while retrieving the entry.
+     *
+     * @returns {*}
+     */
+    get(id: string): any
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     *
+     * `has(id)` returning true does not mean that `get(id)` will not throw an exception.
+     * It does however mean that `get(id)` will not throw a `NotFoundExceptionInterface`.
+     *
+     * @param {string} id Identifier of the entry to look for.
+     *
+     * @throws NotFoundExceptionInterface  No entry was found for **this** identifier.
+     * @throws ContainerExceptionInterface Error while retrieving the entry.
+     *
+     * @returns {boolean}
+     */
+    has(id: string): boolean
+}
+
+export interface ContainerInterface extends PsrContainerInterface {
+    getDefinition(id: string): Definition.Scalar | Definition.Factory
+
+    create(id: string): any
+
+    addScalar(id: string, value: any): this
+
+    addFactory(
+        id: string,
+        factory: (container: ContainerInterface) => any,
+        options?: Options.Factory
+    ): this
+
+    addClass(
+        id: string,
+        constructor: new () => any,
+        options?: Options.Class
+    ): this
+}
+
+/**
+ * @param {string} id
  * @param definition
  * @this {Container}
  */
-function add(id, definition) {
+function add(id: string, definition: Definition.Factory | Definition.Scalar): void {
     if (this.has(id)) {
         throw new AlreadyDefinedException()
     }
@@ -25,7 +107,7 @@ function add(id, definition) {
  * @param {Array} constructorArguments
  * @this {Container}
  */
-function createClass(constructor, constructorArguments) {
+function createClass(constructor, constructorArguments: any[]): object {
     constructorArguments.unshift(constructor);
     const bound = (Function.prototype.bind.apply(constructor, constructorArguments));
 
@@ -36,7 +118,7 @@ function createClass(constructor, constructorArguments) {
  * @param cls
  * @returns {Array}
  */
-function extractArguments(cls) {
+function extractArguments(cls): string[] {
     const matches = cls.prototype.constructor.toString().match(/function\s+[^(]*\(([^)]*)\)/);
     let result = [];
 
@@ -54,7 +136,7 @@ function extractArguments(cls) {
  * @param {*} args
  * @this {Container}
  */
-function resolveConstructorArguments(cls, args) {
+function resolveConstructorArguments(cls, args): any[] {
     let result;
 
     switch (true) {
@@ -93,7 +175,7 @@ export class Container implements ContainerInterface {
         return this._definitions.hasOwnProperty(id)
     }
 
-    getDefinition(id: string): Definition.Scalar | Definition.Class | Definition.Factory {
+    getDefinition(id: string): Definition.Scalar | Definition.Factory {
         if (!this.has(id)) {
             throw new NotFoundException()
         }
@@ -117,25 +199,14 @@ export class Container implements ContainerInterface {
             case Definition.Types.Factory:
                 result = (definition as Definition.Factory).factory(this);
                 break;
-            case Definition.Types.Class:
-                result = createClass.call(
-                    this,
-                    definition.constructor,
-                    resolveConstructorArguments.call(
-                        this,
-                        definition.constructor,
-                        (definition as Definition.Class).options.constructorArguments
-                    )
-                );
-                break;
             default:
                 throw new CreateNotCallableException()
         }
 
         if (
             !this._created.hasOwnProperty(id)
-            && (!(definition as Definition.Class | Definition.Factory).options.hasOwnProperty('createEveryTime')
-            || !(definition as Definition.Class | Definition.Factory).options.createEveryTime)
+            && (!(definition as Definition.Factory).options.hasOwnProperty('createEveryTime')
+            || !(definition as Definition.Factory).options.createEveryTime)
         ) {
             this._created[id] = result
         }
@@ -166,7 +237,18 @@ export class Container implements ContainerInterface {
         }
 
         options || (options = {});
-        add.call(this, id, {type: Definition.Types.Class, constructor, options});
+        this.addFactory(id, function (c: Container) {
+            return createClass.call(
+                c,
+                constructor,
+                resolveConstructorArguments.call(
+                    c,
+                    constructor,
+                    options.constructorArguments
+                )
+            );
+        }, {createEveryTime: options.createEveryTime});
+
         return this
     }
 }
